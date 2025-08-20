e1e01e9630c693cfb512f33c0a717b949d62a2a0
spowmiyasivalingam <sowmiyasivalingam023@gmail.com>
2025-08-20 10:25:52 +0530

    Add files via upload

---
+++ bitwiseor.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = np.zeros((500,500),dtype = np.uint8)
+circle = cv2.circle (img.copy(),(250,250),250,(255,255,255),-1)
+rect = cv2.rectangle(img.copy(),(30,30),(470,470),(255,255,255),-1)
+
+intersection = cv2.bitwise_or(circle,rect,None)
+
+
+plt.imshow(cv2.cvtColor(intersection,cv2.COLOR_BGR2RGB))
+plt.show()
