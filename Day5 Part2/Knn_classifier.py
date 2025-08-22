e4ddc847de5e25db72d640999b66abe7f025eacb
spowmiyasivalingam <sowmiyasivalingam023@gmail.com>
2025-08-22 16:06:18 +0530

    Add files via upload

---
+++ Day5 Part2/Knn_classifier.py
@@ -0,0 +1,37 @@
+import  numpy as np
+import matplotlib.pyplot as plt
+from sklearn import datasets, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+
+#Load digits dataset(8x8 grayscale images)
+digits = datasets.load_digits()
+
+x=digits.images
+y=digits.target
+
+n_samples = len(x)
+X = x.reshape((n_samples, -1))
+
+#Train-Test split
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.5, shuffle=False)
+
+#create and train KNN model(K=5)
+knn = KNeighborsClassifier(n_neighbors= 5)
+knn.fit(X_train, y_train)
+
+#Predictions
+y_pred = knn.predict(X_test)
+
+#Accuracy
+print ("KNN Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+#show a few predictions
+images_and_predictions = list(zip(digits.images[n_samples // 2:], y_pred))
+for index, (image, prediction)in enumerate(images_and_predictions[:4]):
+    plt.subplot(1,4,index + 1)
+    plt.axis("off")
+    plt.imshow(image, cmap=plt.cm.gram)
+    plt.title(f'pred: {prediction}')
+
+plt.show()
